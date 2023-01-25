901e7b1af7dd500d0326e24cf06d4b1c45a012e0
Your Name <you@example.com>
2023-01-25 18:30:34 +0000

    Able to click first button

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,12 +1,33 @@
-const numberButton = document.querySelector("number")
-const equalButton = document.querySelector("equal")
-const decimalButton = document.querySelector("decimal")
-const clearButton = document.querySelector("clear")
+// 
+
+const numberButtons = document.querySelectorAll(".number")
+const operatorButton = document.querySelectorAll(".operator")
+const additionButton = document.querySelector(".addition")
+const equalButton = document.querySelector(".equal")
+const multiplyButton = document.querySelector(".multiply")
+const minusButton = document.querySelector(".minus")
+const decimalButton = document.querySelector(".decimal")
+const clearButton = document.querySelector(".clear")
+
+let currentNumber = "";
 
 
-// Create functions to get HTML classes
+// When number is pressed - display number on screen
+  // addEventListener
+  numberButtons[0].addEventListener('click', (event) => {
+  console.log (event.target.innerHTML)
+  })
 
-// Create variables
-// numbers, operators, display
+// Number on the page
+// All of the buttons on the page
 
-// Give functions to the buttons
+// Show operator on page
+// When operator is pressed -
+  //
+
+// If equal sign is pressed - update display with answer 
+
+//Event is the click, target is the element that the click has happened on, 
+
+
+// If clear button is pressed. Value on screen will return to zero
