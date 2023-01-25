5a47a717a48876037d410eb32da5fab86008ee51
Your Name <you@example.com>
2023-01-25 13:43:57 +0000

    First commit - setting up calc - no JS yet

---
+++ Calculator/script.js
@@ -0,0 +1,12 @@
+const numberButton = document.querySelector("number")
+const equalButton = document.querySelector("equal")
+const decimalButton = document.querySelector("decimal")
+const clearButton = document.querySelector("clear")
+
+
+// Create functions to get HTML classes
+
+// Create variables
+// numbers, operators, display
+
+// Give functions to the buttons
